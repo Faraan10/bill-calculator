9d42bef90c58a6626c206f1e2a7d79eaa4b6d99b
Muhammed <faraanshaik19@gmail.com>
2025-04-23 23:16:46 +0530

    list component commit

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,7 +5,18 @@ const List = ({ friend }) => {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
-      <p>{}</p>
+      {friend.balance < 0 ? (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      ) : friend.balance > 0 ? (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}
+        </p>
+      ) : (
+        <p>both are square</p>
+      )}
+      <button className="button">Select</button>
     </li>
   );
 };
