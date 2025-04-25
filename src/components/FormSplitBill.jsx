7a094b13c5558cc18454257d9b678d202db4e60b
Muhammed <faraanshaik19@gmail.com>
2025-04-25 19:02:24 +0530

    FormSplitBill Updated

--- src/components/FormSplitBill.jsx
+++ src/components/FormSplitBill.jsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const FormSplitBill = ({ selectedFriend }) => {
+  const [data, setData] = useState({
+    bill: "",
+    myAmount: "",
+    paidBy: "user",
+  });
+
+  const { bill, myAmount, paidBy } = data;
+
+  const paidByFriend = bill ? bill - myAmount : "";
+
+  const handleChange = (e) => {
+    setData((prevData) => {
+      const updatedValue =
+        e.target.name === "bill" || e.target.name === "myAmount"
+          ? Number(e.target.value)
+          : e.target.value;
+
+      if (e.target.name === "myAmount" && updatedValue > prevData.bill) {
+        return {
+          ...prevData,
+          myAmount: prevData.bill, // stopping ay bill value
+        };
+      }
+      return {
+        ...prevData,
+        [e.target.name]: updatedValue,
+      };
+    });
+  };
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input type="text" name="bill" value={bill} onChange={handleChange} />
 
       <label>🧍‍♀️ Your expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        name="myAmount"
+        value={myAmount}
+        onChange={handleChange}
+      />
 
       <label>👫 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>💵 Who is paying the bill</label>
-      <select>
+      <select value={paidBy} onChange={handleChange}>
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
